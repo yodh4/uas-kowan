7e3776a78c7388f30ba102381964d5c4642a9afa
yodh4 <cyrilus2004@gmail.com>
2025-12-14 11:32:36 +0700

    update config

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,9 @@ export const rpName: string = 'Kalkulator Lingkaran Passkey';
 
 export const rpID: string = process.env.RP_ID || 'localhost';
 
+const isNgrok = rpID.includes('ngrok');
 const port = process.env.PORT || 8080;
-export const origin: string = `http://${rpID}:${port}`;
+
+export const origin: string = isNgrok 
+    ? `https://${rpID}` 
+    : `http://${rpID}:${port}`;
